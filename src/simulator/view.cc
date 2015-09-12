d9d95e40d14f6de680703e757cb14fb53251cc93
Suriyadeepan Ram <suriyadeepan@acm.org>
2015-09-12 21:02:29 +0530

    start view

---
+++ src/simulator/view.cc
@@ -0,0 +1,54 @@
+#include "opencv2/opencv.hpp"
+#include "stdio.h"
+
+using namespace cv;
+
+
+void view_x4(Mat *view, int SIM_DIM_X, int SIM_DIM_Y){ 
+
+	*view = Mat(SIM_DIM_X*4, SIM_DIM_Y*4, CV_8UC3);
+
+}
+
+
+
+void view_draw_nodes(Mat *view, Point *node_loc, int seed){ /* Remove seed */
+
+	(*view).setTo(cv::Scalar(0,0,0));
+	for(int i=0;i<10;i++){
+		circle( *view, Point( seed + node_loc[i].x * 4, seed + node_loc[i].y * 4), 20, Scalar(150,90,255), -1,8,0);
+
+}
+
+
+int main(){
+
+	//node locations
+	Point node_loc[10];
+	node_loc[0] = Point(26,73);
+	node_loc[1] = Point(34,75);
+	node_loc[2] = Point(89,20);
+	node_loc[3] = Point(55,55);
+	node_loc[4] = Point(10,90);
+	node_loc[5] = Point(90,15);
+	node_loc[6] = Point(50,20);
+	node_loc[7] = Point(75,10);
+	node_loc[8] = Point(30,30);
+	node_loc[9] = Point(30,10);
+
+	Mat view;
+
+	view_x4(&view,200,200);
+
+	int seed = 0;
+	while( seed < 100 ){
+		view_draw_nodes(&view,node_loc,seed);
+		imshow("GUI",view);
+		waitKey(0);
+		seed++;
+	}
+
+	waitKey(0);
+	return 0;
+
+}
---
+++ src/simulator/view.h
@@ -0,0 +1,32 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include "opencv2/opencv.hpp"
+#include "stdio.h"
+
+/* 
+ * We open with a sensor network with multitude of nodes. Actually just 10 nodes, I hate seg fault. Nodes are scattered ackwardly across the network. Let me tell you something about the network. I use the word network synonymously with the area where the sensor nodes are deployed. From a strictly programming perspective, what is a sensor node? Here we omit the "sensor" part; all we care about are that each node is mobile and has a radio capable of transmitting at a fixed power-level (for now) and receiving when within range of another node. 
+ *
+ * As I have mentioned in the root README.md file, the visualizer is built using the MVC pattern at its core. We have built a model that updates every instant of simulation time (sim_t), from the data ( rows in the monolithic log file events.mono) gathered from the events list (events.mono). The model is basically an array of nodes. What is a node? you mean "Node"? Yes, a "Node" is a structure with properties : id, x, y, event_type, sender_node, num_pkt_tx and num_pkt_rx. So, there you have it : your model - an array of "Node"s. 
+ *
+ * Let us move on to View. View handles the display, meaning it draws on to the screen, the nodes (little filled circles) in their respective position. Where do we get their locations? Yes, from the model. As we redraw the view every instant of simulation time (sim_t), the nodes change their locations, giving the viewer an illusion of moving objects (not exactly). What did we miss? the radio communication. At some instants, a number of nodes transmit, a number of nodes receive. We need to put this transmission/reception on the display. I am doing this by using transparent enough circles overlayed on the nodes. Transmission is indicated by a circle of coverage radius of the sender node. Reception is indicated by a circle of radius slightly larger than the receiver node; just large enough for the viewer to notice it. 
+ *
+ * For the initial testing of the view module, assume the following topology[200x200].
+ * 	N1	26,73
+ * 	N2  34,75
+ * 	N3	89,20
+ * 	N4	55,55
+ * 	N5	10,90
+ * 	N6	90,15
+ * 	N7 	50,20
+ * 	N8	75,10
+ * 	N9	30,30
+ * 	N10 30,10
+ *
+ */
+
+
+//void view_x4(SIM_DIM_X,SIM_DIM_Y)
+void view_x4(Mat *,int,int);
+
+#endif
