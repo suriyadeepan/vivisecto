df75e0203b79f4a9f4c5a714708c52a2aa191d2b
Suriyadeepan Ram <suriyadeepan@acm.org>
2015-09-13 10:13:33 +0530

    add key shortcuts : p,+,-,n(frame by frame),q to View

--- src/simulator/view.cc
+++ src/simulator/view.cc
@@ -1,10 +1,9 @@
 #include "view.h"
 
-void view_x4(Mat *view, int SIM_DIM_X, int SIM_DIM_Y){ *view = Mat(SIM_DIM_X*4, SIM_DIM_Y*4, CV_8UC3); }
+void view_x4(Mat *view, int SIM_DIM_X, int SIM_DIM_Y){ *view = Mat(SIM_DIM_X*4, SIM_DIM_Y*4, CV_8UC4); }
 
 void view_drawNodes(Mat *view, Point *node_loc, int seed){ /* Remove seed */
 
-	(*view).setTo(cv::Scalar(0,0,0));
 
 	for(int i=0;i<10;i++){
 		node_loc[i].x = node_loc[i].x + seed;
@@ -12,16 +11,18 @@ void view_drawNodes(Mat *view, Point *node_loc, int seed){ /* Remove seed */
 		circle( *view, Point( node_loc[i].x * 4, node_loc[i].y * 4), 15, Scalar(90,25,25), -1,8,0);
 		char node_id_str[3];
 		sprintf(node_id_str,"%d",i);
-		//putText( *view,node_id_str, Point(-6 + node_loc[i].x * 4, 6 + node_loc[i].y * 4), FONT_HERSHEY_SIMPLEX, 0.6, Scalar(255,255,255), 1,8,false );
+		putText( *view,node_id_str, Point(-6 + node_loc[i].x * 4, 6 + node_loc[i].y * 4), FONT_HERSHEY_SIMPLEX, 0.6, Scalar(20,205,255), 2,8,false );
 	}
 
 }
 
 void view_drawRadioComm(Mat *view, Point *node_loc, int seed1, int seed2){ /* Remove seed */
 
-		circle( *view, Point( node_loc[seed1].x * 4, node_loc[seed1].y * 4), 30, Scalar(20,255,20), -1,8,0);
-		line(*view, Point(node_loc[seed1].x * 4,node_loc[seed1].y * 4),
-			 	Point(node_loc[seed2].x*4,node_loc[seed2].y*4),Scalar(0,255,0), 1,8,0);
+	view->setTo(cv::Scalar(0,0,0));
+
+	circle( *view, Point( node_loc[seed1].x * 4, node_loc[seed1].y * 4), 30, Scalar(20,255,20), -1,8,0);
+	line(*view, Point(node_loc[seed1].x * 4,node_loc[seed1].y * 4),
+			Point(node_loc[seed2].x*4,node_loc[seed2].y*4),Scalar(0,255,0), 1,8,0);
 }
 
 int main(){
@@ -46,12 +47,46 @@ int main(){
 	view_x4(&view,200,200);
 
 	int seed = 1;
-	while( waitKey(80) != 'q' ){
-		view_drawNodes(&view,node_loc,seed);
+	int delay = 80;
+	char user_ip = ' ';
+
+	do{
 		view_drawRadioComm(&view,node_loc,rand()%10,rand()%10);
+		view_drawNodes(&view,node_loc,seed);
     blur( view, view, Size( 5, 5 ) );
 		imshow("GUI",view);
-	}
+
+		user_ip = (char)waitKey(delay);
+
+		switch(user_ip){
+
+			case '+':
+				if(delay > 20)
+					delay -= 50;	
+				else
+					delay = 20;
+				break;
+
+			case '-':
+				if(delay < 200)
+					delay += 50;
+				else
+					delay = 200;
+				break;
+
+			case 'p':
+				if(delay != -1)
+					delay = -1;
+				else
+					delay = 80;
+				break;
+
+			case 'n':
+				delay = -1;
+
+		}
+		
+	}while(user_ip != 'q');
 
 	return 0;
 
