31318b5411991841ab135e04f78ce764c9c18b64
Suriyadeepan Ram <suriyadeepan@acm.org>
2015-09-14 19:45:39 +0530

    motivation for plot module : src/plot/README.md

--- src/map/map.cc
+++ src/map/map.cc
@@ -8,10 +8,10 @@ void map_drawGrid(Mat *map_src){
 	map_src->setTo(Scalar(0,0,0));
 
 	for(int i=0;i<map_src->rows;i+=80)
-		line(*map_src, Point(0,i), Point(SIM_DIM_X,i),Scalar(30,30,30), 1,4,0);
+		line(*map_src, Point(0,i), Point(SIM_DIM_X,i),Scalar(30,30,30), 2,4,0);
 
 	for(int i=0;i<=map_src->cols - 300;i+=80)
-		line(*map_src, Point(i,0), Point(i,SIM_DIM_X),Scalar(30,30,30), 1,4,0);
+		line(*map_src, Point(i,0), Point(i,SIM_DIM_X),Scalar(30,30,30), 2,4,0);
 
 }
 
---
+++ src/plot/README.md
@@ -0,0 +1,3 @@
+# Plot
+
+The Plot module generates data ( from Model ) which is used by GNUPlot to generate plots, like Variation of energy consumed by nodes over simulation time, etc.   
